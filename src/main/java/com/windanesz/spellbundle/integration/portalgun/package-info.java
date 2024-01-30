d2c0384c2ad8de1f916aa4b687a7810e78a0488c
WinDanesz <31292708+windanesz@users.noreply.github.com>
2024-01-30 22:50:47 +0100

    chore: fix typo

--- src/main/java/com/windanesz/spellbundle/integration/portalgun/package-info.java
+++ src/main/java/com/windanesz/spellbundle/integration/portalgun/package-info.java
@@ -2,5 +2,5 @@
  * Never reference any classes from the supported mod outside of this package or outside the {@link com.windanesz.spellbundle.spell} package,
  * or the soft-dependency will break!
  */
-package com.windanesz.spellbundle.integration.treasure2;
+package com.windanesz.spellbundle.integration.portalgun;
 
